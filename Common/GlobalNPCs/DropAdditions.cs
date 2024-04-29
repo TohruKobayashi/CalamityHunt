924277d31881439512c03cff2d89f4cbecc9de2c
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2024-04-29 03:29:19 -0500

    this lemonade be nuclear

--- Common/GlobalNPCs/DropAdditions.cs
+++ Common/GlobalNPCs/DropAdditions.cs
@@ -1,5 +1,7 @@
-﻿using CalamityHunt.Common.DropRules;
+﻿using System.Collections.Generic;
+using CalamityHunt.Common.DropRules;
 using CalamityHunt.Common.Systems;
+using CalamityHunt.Content.Items.Consumable;
 using CalamityHunt.Content.Items.Dyes;
 using CalamityHunt.Content.Items.Misc;
 using CalamityHunt.Content.Items.Misc.AuricSouls;
@@ -26,13 +28,38 @@ namespace CalamityHunt.Common.GlobalNPCs
 
         public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
         {
+            // calamity and calamityless
             if (npc.type == NPCID.QueenSlimeBoss) {
                 npcLoot.Add(ItemDropRule.ByCondition(new RemixWorldDropRule(), ModContent.ItemType<SludgeFocus>()));
             }
+
+            // calamity only
             if (ModLoader.HasMod(HUtils.CalamityMod)) {
                 Mod cal = ModLoader.GetMod(HUtils.CalamityMod);
                 if (npc.type == cal.Find<ModNPC>("Yharon").Type) {
                     npcLoot.Add(ItemDropRule.ByCondition(new YharonSoulDropRule(), ModContent.ItemType<YharonSoul>()));
+                }
+
+                List<ModNPC> plagueEnemies = new List<ModNPC>
+                {
+                    cal.Find<ModNPC>("Melter"),
+                    cal.Find<ModNPC>("PestilentSlime"),
+                    cal.Find<ModNPC>("PlaguebringerMiniboss"),
+                    cal.Find<ModNPC>("PlagueCharger"),
+                    cal.Find<ModNPC>("PlagueChargerLarge"),
+                    cal.Find<ModNPC>("Plagueshell"),
+                    cal.Find<ModNPC>("Viruling")
+                };
+                foreach (ModNPC i in plagueEnemies) {
+                    if (npc.type == cal.Find<ModNPC>(i.Name).Type) {
+                        npcLoot.Add(ItemDropRule.Food(ModContent.ItemType<NuclearLemonade>(), 25));
+                    }
+                }
+            }
+            // calamityless only
+            else {
+                if (npc.type == NPCID.AngryTrapper) {
+                    npcLoot.Add(ItemDropRule.Food(ModContent.ItemType<NuclearLemonade>(), 25));
                 }
             }
         }
