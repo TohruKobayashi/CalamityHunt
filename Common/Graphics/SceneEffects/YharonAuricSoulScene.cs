4a20d21d912d76fbf9e6a5870ef26a573d4c65de
GinYuH <72181395+GinYuH@users.noreply.github.com>
2024-06-05 00:07:09 -0700

    interlude 3 temp disable

--- Common/Graphics/SceneEffects/YharonAuricSoulScene.cs
+++ Common/Graphics/SceneEffects/YharonAuricSoulScene.cs
@@ -1,36 +1,53 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using CalamityHunt.Content.Items.Misc.AuricSouls;
+﻿using CalamityHunt.Content.Items.Misc.AuricSouls;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.Graphics.Effects;
 using Terraria.ModLoader;
-using Terraria.ModLoader.IO;
+using Terraria.ModLoader.Config;
+using System.Linq;
+using System.Reflection;
 
 namespace CalamityHunt.Common.Graphics.SceneEffects;
 
 public class YharonAuricSoulScene : ModSceneEffect
 {
-    public override SceneEffectPriority Priority => SceneEffectPriority.Event;
+    public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
 
     public override int Music => AssetDirectory.Music.YharonSoul;
+
+    private static bool interludeConfigWasOn = false;
 
     public override bool IsSceneEffectActive(Player player)
     {
         bool active = Main.item.Any(n => n.active && n.type == ModContent.ItemType<YharonSoul>());
 
-        if (active && ModLoader.HasMod(HUtils.CalamityMod)) {
-            bool soulPresent = false;
-            Item soul = null;
+        if (ModLoader.HasMod(HUtils.CalamityMod)) {
+            // Disable the interlude 3 config
+            ModConfig calConfig = ModLoader.GetMod(HUtils.CalamityMod).GetConfig("CalamityConfig");
+            PropertyInfo interlude3 = calConfig.GetType().GetProperty("Interlude3", BindingFlags.Instance | BindingFlags.Public);
 
-            for (int i = 0; i < Main.maxItems; i++) {
-                if (Main.item[i].type == ModContent.ItemType<YharonSoul>()) {
-                    soul = Main.item[i];
-                    soulPresent = true;
+            if (active) {
+                if ((bool)interlude3.GetValue(calConfig)) {
+                    interlude3.SetValue(calConfig, false);
+                    interludeConfigWasOn = true;
                 }
-                if (soulPresent) {
-                    Vector2 itemPos = soul.position;
-                    Filters.Scene.Activate("CalamityMod:Yharon", itemPos);
+                bool soulPresent = false;
+                Item soul = null;
+
+                for (int i = 0; i < Main.maxItems; i++) {
+                    if (Main.item[i].type == ModContent.ItemType<YharonSoul>()) {
+                        soul = Main.item[i];
+                        soulPresent = true;
+                    }
+                    if (soulPresent) {
+                        Vector2 itemPos = soul.position;
+                        Filters.Scene.Activate("CalamityMod:Yharon", itemPos);
+                    }
+                }
+            }
+            else {
+                if (interludeConfigWasOn) {
+                    interlude3.SetValue(calConfig, true);
                 }
             }
         }
