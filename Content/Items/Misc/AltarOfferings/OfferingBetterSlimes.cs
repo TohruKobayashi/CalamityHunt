3e04c3ca96b7a51d8ffc174eb0f5a62c303e6aa6
TohruKobayashi <t0hruk0b4y4sh1@gmail.com>
2024-11-16 20:36:18 -0600

    basic offering prereq part 1
    ok i go balance boss now byeeeeee

---
+++ Content/Items/Misc/AltarOfferings/OfferingBetterSlimes.cs
@@ -0,0 +1,52 @@
+﻿using CalamityHunt.Common.Utilities;
+using CalamityHunt.Content.Items.Materials;
+using CalamityHunt.Content.Items.Rarities;
+using CalamityHunt.Content.Tiles;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityHunt.Content.Items.Misc.AltarOfferings
+{
+    public class OfferingBetterSlimes : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 48;
+            Item.height = 48;
+            Item.knockBack = 6;
+            Item.value = 10000;
+            Item.rare = ModContent.RarityType<VioletRarity>();
+            if (ModLoader.HasMod(HUtils.CalamityMod)) {
+                ModRarity r;
+                Mod calamity = ModLoader.GetMod(HUtils.CalamityMod);
+                calamity.TryFind<ModRarity>("Violet", out r);
+                Item.rare = r.Type;
+            }
+            Item.channel = true;
+        }
+
+        public override void AddRecipes()
+        {
+            if (ModLoader.TryGetMod(HUtils.CalamityMod, out Mod calamity)) {
+                CreateRecipe()
+                .AddIngredient(calamity.Find<ModItem>("AscendantSpiritEssence").Type, 10)
+                .AddIngredient(ItemID.Gel, 250)
+                .AddIngredient<GelatinousCatalyst>()
+                .AddTile<SlimeNinjaStatueTile>()
+                .Register();
+            }
+            else {
+                CreateRecipe()
+                .AddIngredient(ItemID.LunarBar, 5)
+                .AddIngredient(ItemID.Gel, 250)
+                .AddIngredient<GelatinousCatalyst>()
+                .AddTile<SlimeNinjaStatueTile>()
+                .Register();
+            }
+        }
+    }
+}
